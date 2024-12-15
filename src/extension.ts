00a67cee6b4910b740bc7ac2ddcf38bad9832d8c
songxianwei <37578783+mulining@users.noreply.github.com>
2024-12-15 15:44:20 +0800

    v0.0.2 - 基于 ssh2-sftp-client 完成鼠标右键选中的文件并上传到指定的服务器对应的多个目录下

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,175 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+// @ts-nocheck
 import * as vscode from 'vscode';
+// import ssh2Utils from './ssh2Utils.js';
+import ssh2Utils from './ssh2-sftp-utils.js';
+import fs from 'fs';
+import path from 'path';
+import { convertDriveLetterToUppercase, mergePath  } from "./utils";
+import { Progress, ProgressLocation } from 'vscode';
+
+// 配置文件路径
+
+// 获取服务器配置信息
+function getConfigs() {
+    return vscode.workspace.getConfiguration('ssh2UploadPlugin') || null;
+}
+
+// 上传指定文件到多个服务器
+async function uploadFileToServerWithContext(localFilePath: string, config) {
+    const localBasePath = config.localBasePath;
+    // vscode.window
+    for (let i = 0; i < config.serverConfigs.length; i++) {
+        const serverConfig = config.serverConfigs[i];
+        const conn = await ssh2Utils.connectSSH(serverConfig);
+        console.log("服务器已经连接成功！");
+        try {
+            const handleRemoteDirPaths = mergePath(localFilePath, localBasePath, serverConfig.remoteDirPaths);
+            console.log('生成的远程的路径：', handleRemoteDirPaths);
+            const uploadPromise = await ssh2Utils.uploadFileToServer(conn, localFilePath, handleRemoteDirPaths);
+            vscode.window.showInformationMessage(`文件 ${localFilePath} 已成功上传到服务器 ${serverConfig.host}`);
+        } catch (err) {
+            vscode.window.showErrorMessage(`上传文件 ${localFilePath} 到服务器 ${serverConfig.host} 时出错：${err.message}`);
+        } finally {
+            await ssh2Utils.closeSSHConnection(conn);
+        }
+    }
+}
+
+/**
+ * 上传指定目录下所有文件到多个服务器
+ * @param localDirPath 指定的目录
+ * @param serverConfigIndices 指定的服务器索引列表
+ * @returns 
+ */
+async function uploadDirectoryContentsToServerWithContext(localDirPath, config) {
+    const localBasePath = config.localBasePath;
+    for (let configItem of config.serverConfigs) {
+        const serverConfig = configItem;
+        const conn = await ssh2Utils.connectSSH(serverConfig);
+        console.log("服务连接成功！");
+        try {
+            const localFiles = await vscode.workspace.fs.readDirectory(vscode.Uri.file(localDirPath));
+            const uploadPromises = [];
+
+            const handleRemoteDirPaths = mergePath(localDirPath, localBasePath, serverConfig.remoteDirPaths);
+            await ssh2Utils.uploadDir(conn, localDirPath, handleRemoteDirPaths);
+            vscode.window.showInformationMessage(`目录 ${localDirPath} 已成功上传到服务器 ${serverConfig.host}`);
+        } catch (err) {
+            vscode.window.showErrorMessage(`上传目录 ${localDirPath} 到服务器 ${serverConfig.host} 时出错：${err.message}`);
+        } finally {
+            await ssh2Utils.closeSSHConnection(conn);
+        }
+    }
+}
+
+// 注册右键菜单命令
+function registerContextMenuCommands() {
+    vscode.commands.registerCommand('ssh2-upload-plugin.uploadFile', async function (uri) {
+        console.log('已注册右键菜单：上传指定的目录', uri);
+        if (uri && uri.fsPath) {
+            let localFilePath = uri.fsPath; // e:\\aaa\\bbb\\ccc
+            // 格式要求： E://aaa/bbb/ccc
+            localFilePath = convertDriveLetterToUppercase(localFilePath).replaceAll('\\', '/');
+            const config = getConfigs();
+            if(!config) {
+                vscode.window.showErrorMessage("缺少配置文件！");
+                return;
+            }
+
+            if (!config.serverConfigs.length) {
+                vscode.window.showErrorMessage('没有可用的服务器配置，请先在设置中添加服务器配置信息。');
+                return;
+            }
+            
+            await uploadFileToServerWithContext(localFilePath, config);
+        }
+    });
+
+    vscode.commands.registerCommand('ssh2-upload-plugin.uploadDirectory', async function (uri) {
+        console.log('已注册右键菜单：上传指定的目录', uri);
+        if (uri && uri.fsPath) {
+            try {
+                
+                let localDirPath = uri.fsPath;
+                localDirPath = convertDriveLetterToUppercase(localDirPath).replaceAll('\\', '/');
+                const config = getConfigs();
+                if(!config) {
+                    vscode.window.showErrorMessage("缺少配置文件！");
+                    return;
+                }
+                if (!config.serverConfigs.length) {
+                    vscode.window.showErrorMessage('没有可用的服务器配置，请先在设置中添加服务器配置信息。');
+                    return;
+                }
+    
+                if(!localDirPath) {
+                    vscode.window.showErrorMessage('请指定要上传的目录或文件！');
+                    return;
+                }
+                
+                await uploadDirectoryContentsToServerWithContext(localDirPath, config);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+
+    vscode.commands.registerCommand('ssh2-upload-plugin.upload', async function(uri) {
+        console.log('ssh 插件 - 注册文件上传菜单');
+        if (uri && uri.fsPath) {
+            let localFilePath = uri.fsPath; // e:\\aaa\\bbb\\ccc
+            // 格式要求： E://aaa/bbb/ccc
+            localFilePath = convertDriveLetterToUppercase(localFilePath).replaceAll('\\', '/');
+            const config = getConfigs();
+            if(!config) {
+                vscode.window.showErrorMessage("缺少配置文件！");
+                return;
+            }
+
+            if (!config.serverConfigs.length) {
+                vscode.window.showErrorMessage('没有可用的服务器配置，请先在设置中添加服务器配置信息。');
+                return;
+            }
+
+            // 创建状态栏项
+            try {
+                const taskFun = async (progress: Progress<{ message?: string; increment?: number }>) => {
+                    const fsStat = fs.lstatSync(localFilePath);
+                    if(fsStat.isFile()) {
+                        await uploadFileToServerWithContext(localFilePath, config);
+                    } else {
+                        await uploadDirectoryContentsToServerWithContext(localFilePath, config);
+                    }
+                };
+
+                vscode.window.withProgress({
+                    location: ProgressLocation.Notification,
+                    title: "ssh2正在执行上传任务, 请稍等..."
+                }, taskFun);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+
+    vscode.commands.registerCommand('ssh2-upload-plugin.editConfig', async function () {
+        console.log('已注册右键菜单：配置读取');
+        const configFilePath: string | undefined = vscode.workspace.getConfiguration('ssh2-upload-plugin').get('configFile');
+		if(!configFilePath) {
+			return;
+		}
+        const document = await vscode.workspace.openTextDocument(configFilePath);
+        const editor = await vscode.window.showTextDocument(document);
+        // 这里可以添加更多针对编辑配置文件的交互逻辑，比如保存时的验证等
+    });
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	registerContextMenuCommands();
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ssh-publish" is now active!');
@@ -13,14 +177,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ssh-publish.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const message = "Hello vscode!";
-		vscode.window.showInformationMessage(message);
-	});
+	// const disposable = vscode.commands.registerCommand('ssh-publish.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	const message = "Hello vscode!";
+	// 	vscode.window.showInformationMessage(message);
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
--- src/ssh2-sftp-utils.js
+++ src/ssh2-sftp-utils.js
@@ -1,37 +1,19 @@
-import { Client } from 'ssh2';
+// import { Client } from 'ssh2';
+import Client from 'ssh2-sftp-client';
 import path from 'path';
 // const { Client } = require('ssh2');
 // const path = require('path');
-
+// const conn = new Client();
 // 创建一个包含所有SSH相关工具方法的对象
 const ssh2Utils = {
     // 连接到SSH服务器并返回连接对象（支持async/await）
     connectSSH: async function (serverConfig) {
+        console.log("准备连接ssh");
         const conn = new Client();
         console.log("config service", serverConfig);
-        return new Promise((resolve, reject) => {
-            conn.on('ready', () => {
-                console.log('- ssh 链接成功...');
-                resolve(conn);
-            });
-            conn.on('error', (err) => {
-                reject(err);
-            });
-            conn.connect(serverConfig);
-        });
-    },
-
-    // 获取SFTP对象（支持async/await）
-    getSFTP: async function (conn) {
-        return new Promise((resolve, reject) => {
-            conn.sftp((err, sftp) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(sftp);
-                }
-            });
-        });
+        await conn.connect(serverConfig);
+        console.log("连接成功！");
+        return conn;
     },
 
     // 在服务器上检查远程目录是否存在（支持async/await）
@@ -74,14 +56,14 @@ const ssh2Utils = {
                                 sftp.mkdir(currentDir, (err) => {
                                     if (err) {
                                         if (err.code === 'EACCES') {
-                                            rejectStat(new Error(`- 在服务器 ${conn.config.host} 上创建文件夹 ${currentDir} 时权限不足，请检查相关权限设置。`));
+                                            rejectStat(new Error(`- 在服务器 "指定服务器ip地址的" 上创建文件夹 ${currentDir} 时权限不足，请检查相关权限设置。`));
                                         } else if (err.code === 'ENOENT' || err.code == 2) {
                                             rejectStat(new Error(`- 创建文件夹 ${currentDir} 时，上级目录不存在，请检查目标路径是否正确。`));
                                         } else {
                                             rejectStat(err);
                                         }
                                     } else {
-                                        console.log(`- 在服务器 ${conn.config.host} 上创建文件夹 ${currentDir}`);
+                                        console.log(`- 在服务器 "指定服务器ip地址的" 上创建文件夹 ${currentDir}`);
                                         resolveStat();
                                     }
                                 });
@@ -103,52 +85,72 @@ const ssh2Utils = {
     },
 
     // 通过SFTP上传文件到服务器，支持async/await）
-    uploadFileToServer: async function (conn, localFilePath, remoteDirPaths) {
-        const sftp = await this.getSFTP(conn);
+    uploadFileToServer: async function (conn, localFilePath, remoteFilePaths) {        
         // 创建一个数组来存储所有上传任务的Promise
-        const uploadPromises = [];
-
-        for (let i = 0; i < remoteDirPaths.length; i++) {
-            const remoteDirPath = remoteDirPaths[i];
-            const remoteFilePath = `${remoteDirPath}${localFilePath}`;
-            const localFileP = `${process.env.LOCAL_PATH}/${localFilePath}`;
+        // const uploadPromises = [];
+        for (let i = 0; i < remoteFilePaths.length; i++) {
+            const remoteFilePath = remoteFilePaths[i];
             
-            const uploadPromise = new Promise((resolve, reject) => {
-                sftp.fastPut(localFileP, remoteFilePath, (err) => {
-                    if (err) {
-                        console.log(err)
-                        if (err.code === 'ENOENT' || err.code == 2) {
-                            reject(new Error(`- 文件 ${localFileP} 在本地不存在，请检查路径是否正确。`));
-                        } else if (err.code === 'EPERM') {
-                            reject(new Error(`- 上传文件 ${localFileP} 到服务器 ${conn.config.host} 时权限不足，请检查相关权限设置。`));
-                        } else {
-                            reject(err);
-                        }
-                    } else {
-                        console.log(`- 文件 ${localFileP} 已成功上传到服务器 ${conn.config.host}${remoteFilePath}`);
-                        resolve();
-                    }
-                });
-            });
+            try {
+                // 分割路径以获取目录部分
+                const dirname = path.dirname(remoteFilePath);
+                // 检查目录是否存在
+                try {
+                    console.log('文件路径：', dirname);
+                    const status = await conn.stat(dirname);
+                    console.log('获取文件是否存在：', status);
+                } catch (err) {
+                    // 目录不存在，创建目录
+                    const res = await conn.mkdir(dirname, true);
+                    console.log('目录创建结果：', res);
+                }
 
-            uploadPromises.push(uploadPromise);
+                await conn.fastPut(localFilePath, remoteFilePath);
+                console.log(`- 文件 ${localFilePath} 已成功上传到服务器 ${remoteFilePath}`);
+            } catch (err) {
+                if (err) {
+                    console.log(err);
+                    if (err.code === 'ENOENT' || err.code === 2) {
+                        throw new Error(`- 文件 ${localFilePath} 在本地不存在，请检查路径是否正确。`);
+                    } else if (err.code === 'EPERM') {
+                        throw new Error(`- 上传文件 ${localFilePath} 到服务器 "指定服务器ip地址的" 时权限不足，请检查相关权限设置。`);
+                    } else {
+                        throw err;
+                    }
+                }
+            }
         }
 
         // 使用Promise.all来并行等待所有上传任务完成
-        return await Promise.all(uploadPromises);
+        // return await Promise.all(uploadPromises);
+    },
+
+    /**
+     * 上传指定目录到服务器
+     * @param {*} conn 服务器对象
+     * @param {*} localDirPath 本地路径
+     * @param {*} remoteDirPath 远程路径
+     * @returns 
+     */
+    uploadDir: async function (conn, localDirPath, remoteDirPaths) {
+        for (let i = 0; i < remoteDirPaths.length; i++) {
+            const remoteDirPath = remoteDirPaths[i];
+            try {
+                await conn.uploadDir(localDirPath, remoteDirPath);
+            } catch (error) {
+                throw error;
+            }
+        }
     },
 
     // 关闭SSH连接，支持async/await）
     closeSSHConnection: async function (conn) {
-        await new Promise((resolve, reject) => {
-            conn.end((err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        });
+        try {
+            conn.end();
+            console.log('ssh2 连接已关闭！');
+        } catch (error) {
+            throw error;
+        }
     },
 
     // 处理传入路径，若最后是文件则去掉文件名只保留文件夹层级
